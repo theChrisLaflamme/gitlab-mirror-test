f5190c94d35d6de5c5f057d9c707487a3423408d
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-19 21:26:47 +0000

    Brew formula update for gorereleaser version v0.1.14

--- Formula/gorereleaser.rb
+++ Formula/gorereleaser.rb
@@ -5,29 +5,29 @@
 class Gorereleaser < Formula
   desc "Software to create fast and easy drum rolls."
   homepage "https://support.cimpress.cloud/hc/en-us/articles/227873908"
-  version "0.1.12"
+  version "0.1.14"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://gitlab.com/28251648/-/releases/v0.1.12/downloads/gorereleaser_0.1.12_Darwin_x86_64.tar.gz"
-      sha256 "9a7ef9f0dec1f2e5e19101e3f5b4bb5a404d5003b8ad26fc919aaa2fb65f0788"
+      url "https://gorereleaser-tst.s3.us-east-1.amazonaws.com/gorereleaser/v0.1.14/gorereleaser_0.1.14_Darwin_x86_64.tar.gz"
+      sha256 "9cdf6980f68f86254d18bfc4ddf114eb6f5fc606aca268d77002af01a885bf6c"
     end
     if Hardware::CPU.arm?
-      url "https://gitlab.com/28251648/-/releases/v0.1.12/downloads/gorereleaser_0.1.12_Darwin_arm64.tar.gz"
-      sha256 "12d5999daa6e79c63928ef7f3adf67e96782b30fe4d8aeb3d08f37f6bea5fcb6"
+      url "https://gorereleaser-tst.s3.us-east-1.amazonaws.com/gorereleaser/v0.1.14/gorereleaser_0.1.14_Darwin_arm64.tar.gz"
+      sha256 "30ff3c3958bd0eab7920d243002806d936e8b63675f95e6ef7fea756125551f3"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://gitlab.com/28251648/-/releases/v0.1.12/downloads/gorereleaser_0.1.12_Linux_x86_64.tar.gz"
-      sha256 "f8687d2bc6e3bf2d6d0d21cd5fa9fa926f377fe844fb3e53b09ff87e5c9099f8"
+      url "https://gorereleaser-tst.s3.us-east-1.amazonaws.com/gorereleaser/v0.1.14/gorereleaser_0.1.14_Linux_x86_64.tar.gz"
+      sha256 "79dd33f9d340122b8f14ee0ba3eadc2aa4f51c0c9f6c8eb4213b907acb5e7a2e"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://gitlab.com/28251648/-/releases/v0.1.12/downloads/gorereleaser_0.1.12_Linux_arm64.tar.gz"
-      sha256 "87de3fda9c31aa3ea8429d9ebebe116bdbe56a862aad66775806d26c6c7850f3"
+      url "https://gorereleaser-tst.s3.us-east-1.amazonaws.com/gorereleaser/v0.1.14/gorereleaser_0.1.14_Linux_arm64.tar.gz"
+      sha256 "fc84d08327c505eef20d219e34c198d000060d02755791345e8674ba1474105f"
     end
   end
 
